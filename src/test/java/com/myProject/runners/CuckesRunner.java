07d7c710f8dddf11278cff3a686a67bc6e0fc064
Evelin <evelindacanovas@gmail.com>
2021-08-11 17:38:04 +0200

    CukesRunner ready

--- src/test/java/com/myProject/runners/CuckesRunner.java
+++ src/test/java/com/myProject/runners/CuckesRunner.java
@@ -1,4 +1,21 @@
 package com.myProject.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+                // "html:target/default-html-features" ->to get report.It override itself. No need if we use json
+        },
+        features = "src/test/resources/features",
+        glue = "com/vytrack/step_definitions",
+        dryRun = false,
+        tags = "@wip"
+)
+
 public class CuckesRunner {
 }
