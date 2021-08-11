114fee2eee396bc2a36f4bc53263c53311f66ece
Evelin <evelindacanovas@gmail.com>
2021-08-11 20:43:33 +0200

    shiva bla commint

--- src/test/java/com/myProject/steps_definitions/forShivaStepsDefs.java
+++ src/test/java/com/myProject/steps_definitions/forShivaStepsDefs.java
@@ -1,9 +1,14 @@
 package com.myProject.steps_definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class forShivaStepsDefs {
     @Given("bla")
     public void bla() {
+    }
+
+    @When("blabla")
+    public void blabla() {
     }
 }
