1cad239bdf6765a7d516d21c405ad4151b5fd305
Evelin <evelindacanovas@gmail.com>
2021-08-11 20:36:55 +0200

    bla bla bla feature for shiva and steps defs

---
+++ src/test/java/com/myProject/steps_definitions/forShivaStepsDefs.java
@@ -0,0 +1,9 @@
+package com.myProject.steps_definitions;
+
+import io.cucumber.java.en.Given;
+
+public class forShivaStepsDefs {
+    @Given("bla")
+    public void bla() {
+    }
+}
---
+++ src/test/resources/features/forShiva.feature
@@ -0,0 +1,5 @@
+Feature: Shiva
+  Scenario: lala
+    Given bla
+    When bla
+    Then bla
